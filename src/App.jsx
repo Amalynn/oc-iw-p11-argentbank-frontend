1803112d8e4db1e11305282c7211fe12bb38bf0d
Amalynn <amandine.ebner@gmail.com>
2024-06-04 09:32:56 +0200

    React Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import SignIn from './pages/SignIn';
+import Error404 from './pages/Error404';
+import UserAccount from './pages/UserAccount';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from 'react-router-dom';
 
 export default function App() {
     return (
-        <>
-            <HomePage />
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/user-account" element={<UserAccount />} />
+                <Route path="/error404" element={<Error404 />} />
+                <Route path="*" element={<Navigate to="/error404" replace />} />
+            </Routes>
+
             <Footer />
-        </>
+        </Router>
     );
 }
